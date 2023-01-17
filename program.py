936f9e83f48a3d976a5b8df865ce80a1776658a1
alaan8 <aleksey.trusov07@gmail.com>
2023-01-17 16:30:32 +0300

    First Commit

--- program.py
+++ program.py
@@ -1,9 +1,5 @@
-def main():
-    print('My first git program')
-    print('And I change it every day')
-    print('Again')
-    print('UFO came and added this line')
+import pygame
 
+size = [800, 1000]
 
-if __name__ == '__main__':
-    main()
+screen = pygame.display.set_mode(size)
